4bd51e88ea736685d7f607eacd686376e38f4f1e
Strelkova Olga <strelod@yandex.ru>
2022-07-25 14:04:25 +0300

    feat api

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+class Api {
+  constructor(config) {
+    this._baseUrl = config.baseUrl;
+  }
+
+
+getCards() {
+    return fetch(`${this._baseUrl}beers`)
+      .then(res => res.ok ? res.json() : Promise.reject(res.status))
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://api.punkapi.com/v2/",
+});
+
+export default api;
