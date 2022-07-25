310052cbb8382b0b465b0f6bb5dd7cc994a2e521
Strelkova Olga <strelod@yandex.ru>
2022-07-25 19:41:58 +0300

    feat get carditem api

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
 import api from "../api/api";
@@ -12,10 +12,24 @@ import Popup from "./Popup";
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [cardsData, setCardsData] = useState([]);
-  const [selectedCard, handleCardClick] = useState(null);
+  const [selectedCardId, handleCardClick] = useState(null);
+  const [popupData, setPopupData] = useState(null);
+
+  useEffect(() => {
+    api.getCardById(selectedCardId).then((res) => {
+      setPopupData({
+        link: res[0].image_url,
+        title: res[0].name,
+        subtitle: res[0].description,
+        tagline: res[0].tagline,
+        abv: res[0].abv,
+        food: res[0].food_pairing,
+      });
+    });
+  }, [selectedCardId]);
 
   const closePopup = () => {
-    handleCardClick(null);
+    setPopupData(null);
   };
 
   const handleSubmit = (e) => {
@@ -47,7 +61,7 @@ function App() {
             <Card {...card} key={card.id} onCardClick={handleCardClick} />
           ))}
         </section>
-        <Popup selectedCard={selectedCard} onClose={closePopup} />
+        <Popup popupData={popupData} onClose={closePopup} />
       </div>
     </div>
   );
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,11 +3,11 @@ import React from "react";
 import "../styles/Popup.css";
 
 function Popup(props) {
-  const { selectedCard, onClose } = props;
-  console.log(selectedCard)
+  const { popupData, onClose } = props;
+  console.log(popupData?.title);
   return (
-    <div className={`Popup ${selectedCard && "Popup-open"}`}>
-      <div className="popup__container popup__container_type_img">
+    <div className={`Popup ${popupData && "Popup-open"}`}>
+      <div className="Popup-container">
         <button
           type="button"
           className="popup__close popup__close_type_big-image"
@@ -17,11 +17,11 @@ function Popup(props) {
 
         <img
           className="popup__image"
-          src={selectedCard?.link}
-          alt={selectedCard?.name}
+          src={popupData?.link}
+          alt={popupData?.title}
         />
 
-        <p className="popup__caption">{selectedCard?.name}</p>
+        <p className="popup__caption">{popupData?.title}</p>
       </div>
     </div>
   );
